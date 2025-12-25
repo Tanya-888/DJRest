3ee9184aa60b8666e55cbc1846dc4a0aa9eb5362
Tanya-888 <vileyka.box@gmail.com>
2025-12-25 12:47:12 +0300

    dpr

--- training_tracker/training_tracker/urls.py
+++ training_tracker/training_tracker/urls.py
@@ -19,12 +19,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
 
-# Создайте view для главной
+
 def index(request):
     return render(request, 'index.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
-    path('', index),  # главная страница
+    path('', index),
 ]
