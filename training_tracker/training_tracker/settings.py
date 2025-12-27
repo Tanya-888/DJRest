3c6f03ad13347a23399124f5fae04375f66d46d4
Tanya-888 <vileyka.box@gmail.com>
2025-12-27 18:00:51 +0300

    dpr

--- training_tracker/training_tracker/settings.py
+++ training_tracker/training_tracker/settings.py
@@ -17,26 +17,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 AUTH_USER_MODEL = 'core.User'
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-or7z8bd$*nj2^o3cag4u!6p_t@&fhs2fa=*)r9p$_myuz71ult'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-# Application definition
 
 INSTALLED_APPS = [
 
@@ -82,8 +73,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'training_tracker.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -96,8 +85,6 @@ DATABASES = {
     }
 }
 
-# Password validation
-# https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -115,8 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/6.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -127,7 +112,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
+
 
 STATIC_URL = 'static/'
