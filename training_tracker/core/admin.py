e6b87b9fc8073fb7872b4817243be15dcdf87322
Tanya-888 <vileyka.box@gmail.com>
2025-12-27 17:58:59 +0300

    dpr

--- training_tracker/core/admin.py
+++ training_tracker/core/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import User, CoachProfile, WorkoutProgram, Exercise, UserWorkoutPlan, ExerciseResult, Reminder
+
+admin.site.register(User)
+admin.site.register(CoachProfile)
+admin.site.register(WorkoutProgram)
+admin.site.register(Exercise)
+admin.site.register(UserWorkoutPlan)
+admin.site.register(ExerciseResult)
+admin.site.register(Reminder)
