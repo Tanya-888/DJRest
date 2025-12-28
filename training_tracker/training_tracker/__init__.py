7a679c5dd69c74d283e2cb8c829c7e6f59c23dbc
Tanya-888 <vileyka.box@gmail.com>
2025-12-28 15:32:23 +0300

    dpr

--- training_tracker/training_tracker/__init__.py
+++ training_tracker/training_tracker/__init__.py
@@ -1,15 +1,5 @@
-from __future__ import absolute_import
+from __future__ import absolute_import, unicode_literals
 
-import os
-from celery import Celery
+from .celery import app as celery_app
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'training_tracker.settings')
-
-app = Celery('training_tracker')
-
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
-
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
+__all__ = ['celery_app']
